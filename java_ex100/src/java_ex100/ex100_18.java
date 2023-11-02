1fe3fb3c39b50a95267f3b5910902e10c39dc772
YOON331 <yb97pe@gmail.com>
2023-11-02 16:33:44 +0900

    ex7~ex20

---
+++ java_ex100/src/java_ex100/ex100_18.java
@@ -0,0 +1,14 @@
+package java_ex100;
+import java.util.Scanner;
+
+public class ex100_18 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		char c = scan.nextLine().charAt(0);
+		
+		System.out.printf("%d", (int) c);
+
+	}
+
+}
---
+++ java_ex100/src/java_ex100/ex100_20.java
@@ -0,0 +1,19 @@
+package java_ex100;
+import java.util.Scanner;
+
+public class ex100_20 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		int num3 = scan.nextInt();
+		int sum = num1+num2+num3;
+		float avg = (float)sum/3;
+		
+		System.out.printf("%d\n",sum);
+		System.out.printf("%.1f\n", avg);
+
+	}
+
+}
